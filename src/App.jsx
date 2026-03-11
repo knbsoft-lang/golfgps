7693a63772edf839c6087cc94528e386985d0718
knbsoft-lang <knbsoft@yahoo.com>
2026-03-11 17:19:33 -0400

    01

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { holeImagePath } from "./data/holeImages";
 import { getHoleDefaults } from "./data/holeDefaults";
 
 const TEE_BOXES = ["Black", "Gold", "Blue", "White", "Green", "Red", "Friendly"];
-const TEST_SYNC_ID = "TEST-05";
+const TEST_SYNC_ID = "TEST-01";
 
 // ✅ AUTO BUILD ID (changes every time you run `npm run build`)
 const BUILD_TEST_ID =
